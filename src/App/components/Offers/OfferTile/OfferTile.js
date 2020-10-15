1de99fb334cb55ce736a3ee27065d981efaa5e2a
przemo9696 <przemek.iskra96@gmail.com>
2020-10-15 18:25:42 +0200

    WIP

--- src/App/components/Offers/OfferTile/OfferTile.js
+++ src/App/components/Offers/OfferTile/OfferTile.js
@@ -18,7 +18,21 @@ const OfferTile = (props) => {
     <div className={styles.offerTile}>
       <div className={`${styles.techColorContainer} ${technologyColor}`}></div>
       <div className={styles.logoContainer}>{props.logo}</div>
-      <div className={styles.offerInfo}>data</div>
+      <div className={styles.offerInfoContainer}>
+        <div className={styles.offerInfo_top}>
+          <div className={styles.jobTitleContainer}>
+            <div className={styles.jobTitle}>Junior Front-End Developer</div>
+          </div>
+          <div className={styles.jobSalaryContainer}>
+            <span className={styles.jobSalary}>3 500 - 4 500</span>
+            <div className={styles.releaseTime}>New</div>
+          </div>
+        </div>
+        <div className={styles.offerInfo_bottom}>
+          <div className={styles.companyDetails}>Company</div>
+          <div className={styles.techDetails}>JavaScript</div>
+        </div>
+      </div>
     </div>
   );
 };
